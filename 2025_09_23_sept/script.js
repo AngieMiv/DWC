87d18a99a6ab47dc8e4223c557cc2bfdd6076aac
AngieMiv <angieibarrola@gmail.com>
2025-10-01 01:04:13 +0200

    loops - bucles

--- 2025_09_23_sept/script.js
+++ 2025_09_23_sept/script.js
@@ -1,20 +1,100 @@
-/**
- * Bucle para calcular 253 (máxima precisión entera para JS).
- * Muestra también 253 + 1.
- * Con un bucle calcula 2128 (número de IPv6 disponibles)
- * Calcula el factorial de 128.
- * b) Haz los mismos cálculos utilizando números BigInt
- */
-let base = 2;
-let resultado = 1;
+// a) Haz los siguientes ejercicios utilizando bucles diferentes
 
-for (let i = 0; i <= 53; i++) {
-		resultado *= base;
+// 1) Bucle para calcular 2^53 (máxima precisión entera para JS).
+let n = 2;
+let x = 53;
+let r1 = 1;
+
+for(let i=1; i<=x; i++){
+    r1 = r1 * n;
 }
+document.getElementById("r1").innerHTML 
+= "El resultado es: " + r1;
 
-document.getElementById("r53").innerHTML = resultado;
-document.getElementById("r531").innerHTML = resultado + 1;
+// 2) Muestra también 2^53 + 1.
+let r2 = 1;
 
+let i = x;
+while (i > 0) {
+    r2 = r2 * n;
+    i--;
+}
+r2 = r2 + 1;
+
+document.getElementById("r2").innerHTML 
+= "El resultado es: " + r2 ;
+
+// 3) Con un bucle calcula 2^128 (número de IPv6 disponibles)
 /**
- * Cálculo de 2^53 con bucle for con BitInt
+ * Las direcciones IPv6 tienen una longitud de 128 bits, 
+ * y cada bit puede tener dos estados (0 o 1). 
+ * Al tener 128 bits, el número total de combinaciones posibles de 
+ * bits (y, por lo tanto, de direcciones) es 2 
+ * multiplicado por sí mismo 128 veces (2^128)
  */
+let state = 2;
+let bits = 128;
+let r3 = 1;
+
+do {
+    r3 = r3 * state;
+    bits--;
+} while (bits > 0);
+document.getElementById("r3").innerHTML 
+= "El resultado es: " + r3;
+
+// 4) Calcula el factorial de 128.
+let b = 128;
+let r4 = 1;
+
+for (let i4 = 1; i4 <= b; i4++) {
+    r4 = r4 * i4;
+}
+document.getElementById("r4").innerHTML 
+= "El resultado es: " + r4;
+
+// b) Haz los mismos cálculos utilizando números BigInt utilizando bucles diferentes
+
+// 5) Calcular 2^53
+let nb = 2n;
+let xb = 53n;
+let r5 = 1n;
+
+for (let ib = 1n; ib <= xb; ib++) {
+    r5 = r5 * nb;
+}
+document.getElementById("r5").innerHTML 
+= "El resultado es: " + r5;
+
+// 6) 2^53 + 1
+let r6 = 1n;
+
+let ib = xb
+while (ib > 0n) {
+    r6 = r6 * nb;
+    ib--;
+}
+r6 = r6 + 1n;
+document.getElementById("r6").innerHTML 
+= "El resultado es: " + r6 ;
+
+// 7) 2^128 (nº de IPv6 disponibles)
+let stateN = 2n;
+let bitsN = 128n;
+let r7 = 1n;
+
+do {
+    r7 = r7 * stateN;
+    bitsN--;
+} while (bitsN > 0n);
+document.getElementById("r7").innerHTML 
+= "El resultado es: " + r7;
+
+// 8) Factorial de 128
+let r8 = 1n;
+
+for (let i=1n; i<=b; i++) {
+    r8 = r8 * i;
+}
+document.getElementById("r8").innerHTML 
+= "El resultado es: " + r8;
